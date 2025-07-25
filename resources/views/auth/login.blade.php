90d8fd46d700d07cb824c3956a8ceb01bcf85240
liffet <alifnazar4@gmail.com>
2025-07-25 14:17:37 +0700

    tes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
         @csrf
 
         <!-- Email Address -->
-        <div class="">
+        <div class="mb-4 mt-4 p-8">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
